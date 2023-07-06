356e7d559b713d3cfe9d214aadae7bf414c41d00
nickforster <nick_forster@icloud.com>
2023-07-06 14:40:42 +0200

    created home page file

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../model/stats.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var stats = Provider.of<Stats>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Touch the screen and flip the phone!"),
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: GestureDetector(
+        onTap: phoneThrown,
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Phone flips X: ${stats.getFlipX()}"),
+                Text("Phone flips Y: ${stats.getFlipY()}"),
+                Text("Phone flips Z: ${stats.getFlipZ()}"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void phoneThrown() {}
+}
